787586af57cdad3f3f3aebfe08c41336db52c602
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-02-14 13:47:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+var btn = document.getElementById("swap");
+var app = document.getElementById("app");
+
+var isDark = false;
+function swapTheme(){
+	if(!isDark){
+		app.style.backgroundColor = 'black' 
+		isDark = true;
+	}
+	else {
+		app.style.backgroundColor = 'white'
+		isDark = false;
+	}
+}
+
+btn.addEventListener('click',swapTheme)
 
